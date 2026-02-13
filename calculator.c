3ac247dedf923604b83431c56350d87e52044d4c
mohanedbecheikh-byte <mohaned.becheikh@univ-alger.dz>
2026-02-13 16:43:13 +0100

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Simple C Calculator\n");
+    printf("-------------------\n");
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+
+        case '-':
+            result = num1 + num2;
+            break;
+
+        case '*':
+            result = num1 * num2;
+            break;
+
+        case '/':
+            if(num2 != 0)
+                result = num1 / num2;
+            else
+                printf("Error: Division by zero.\n");
+            break;
+
+        default:
+            printf("Invalid operator.\n");
+    }
+
+    printf("Result: %.0lf\n", result);
+
+    return 1;
+}
